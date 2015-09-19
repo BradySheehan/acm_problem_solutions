d3e3c67de4142ad4e437d691bb5d4529896049c0
Brady Sheehan <Bradya.sheehan@gmail.com>
2015-09-19 09:15:53 -0400

    Solutions to more ACM Problems
    Java solutions to the Fridge Magnet problem and the Mad scientist
    problem.

---
+++ fridge_magnets/magnets.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class magnets {
+
+	public static void main(String[] args) {
+		File file = new File("/Users/BradySheehan/Documents/Development/java/problem_solving/FridgeMagents/src/magnets_input.txt");
+		try{  
+			Scanner sc = new Scanner(file);
+			String line = null;
+			while(sc.hasNextLine()){
+				line = sc.nextLine();
+				if(line.equals("END"))
+					break;
+				boolean good = true;
+				for(int i = 0; i < line.length(); i++){
+					if(line.charAt(i) != ' '){
+						char c = line.charAt(i);
+						for(int j = 0; j < line.length() && j!=i; j++){
+							if(line.charAt(i) != ' ' ){
+								if(c == line.charAt(j)){
+									good = false; 
+									break;
+								}
+							}
+						}
+					}
+				}
+				if(good){
+					System.out.println(line);
+				}
+			}
+		}catch(IOException e){System.err.println(e.getMessage());}
+	}
+
+}
